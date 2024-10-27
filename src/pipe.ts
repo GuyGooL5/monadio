aec43c25ecd3d797d42ac6046f10c2939a76aeef
Guy Tsitsiashvili <guygool4@gmail.com>
2024-10-27 15:30:22 +0200

    feat: implement and document Procedure and Pipe

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,35 +1,122 @@
 import { Maybe } from "./maybe";
+import type { Procedure } from "./procedure";
 import { Result } from "./result";
 
 type PipeFn<T, U> = (value: T) => U;
 
+/**
+ * Pipe is a wrapper class around a value that allows you to chain transformations and effects in a functional way.
+ *
+ * @remarks if you need a lazy flow of data transformation, consider using {@link Procedure} instead.
+ *
+ * To create a Pipe, use the static method {@link Pipe.create} followed by chaining the methods {@link Pipe.map} and {@link Pipe.effect} to eagerly transform and produce side effects on the value.
+ * To finalize the Pipe, you can unwrap the value using the {@link Pipe.unwrap} method, or you can convert it into a {@link Maybe} or {@link Result} using the {@link Pipe.intoMaybe} and {@link Pipe.intoResult} methods.
+ *
+ * ---
+ * Here's a simple example of how using Pipe differs from using vanilla code:
+ *
+ * vanilla code:
+ * ```typescript
+ * declare const value: number;
+ * const valuePlusOne = value + 1;
+ * console.log("add One:", valuePlusOne);
+ * const valueDoubled = valuePlusOne * 2;
+ * const valueToFixed = valueDoubled.toFixed(2);
+ * console.log("doubled and toFixed:", valueToFixed);
+ * const result = valueToFIxed // this is not mandatory, but it's here to show we're done
+ * ```
+ * using Pipe:
+ * ```typescript
+ * declare const value: number;
+ * const result = Pipe.create(value)
+ * 	.map((value) => value + 1)
+ * 	.effect((value) => console.log("add One:", value))
+ * 	.map((value) => value * 2)
+ * 	.map((value) => value.toFixed(2))
+ * 	.effect((value) => console.log("doubled and toFixed:", value))
+ * 	.unwrap();
+ * ```
+ * As you can see the flow of data in the Pipe is more explicit and contained (no need to declare intermediate variables and pollute the scope).
+ */
 export class Pipe<T> {
 	private readonly value: T;
 	private constructor(value: T) {
 		this.value = value;
 	}
 
+	/**
+	 * Wraps a value in a Pipe instance.
+	 * @param value - the value to wrap in a Pipe.
+	 * @returns a new Pipe instance.
+	 */
 	static create<T>(value: T): Pipe<T> {
 		return new Pipe(value);
 	}
 
+	/**
+	 * Maps the value of the Pipe to a new value using a transformation function.
+	 * @param mapFn - the transformation function.
+	 * @returns a new Pipe instance with the transformed value.
+	 */
 	public map<U>(mapFn: PipeFn<T, U>): Pipe<U> {
 		return new Pipe(mapFn(this.value));
 	}
 
+	/**
+	 * Applies a side effect to the value of the Pipe.
+	 * @param effectFn - the side effect function.
+	 * @returns a new Pipe instance with the same value as before.
+	 */
 	public effect(effectFn: (value: T) => void): Pipe<T> {
 		effectFn(this.value);
-		return this;
+		return new Pipe(this.value);
 	}
 
+	/**
+	 * Unwraps the value of the Pipe (extracts the value from the Pipe as plain data).
+	 * @returns the value of the Pipe.
+	 */
 	public unwrap(): T {
 		return this.value;
 	}
 
+	/**
+	 * Converts the value of the Pipe into a {@link Maybe} instance.
+	 * @returns a new Maybe instance with the value of the Pipe.
+	 * @remarks
+	 * - This method is a equivalent to `Maybe.from(pipe.unwrap())`.
+	 * - **Important**: although this method is completely fine, perhaps there is a way to inhernetly start with a Maybe instead of a Pipe, which would be more idiomatic.
+	 * @see {@link Maybe}
+	 * @see {@link Maybe.from}
+	 *
+	 * @example
+	 * ```typescript
+	 * type User = { name: string };
+	 * declare const user: User | null;
+	 * const maybeUserName = Pipe.create(user).map((user) => user?.name).intoMaybe();
+	 * // 	^? Maybe<string>
+	 * ```
+	 */
 	intoMaybe() {
 		return Maybe.from(this.value);
 	}
 
+	/**
+	 * Converts the value of the Pipe into a {@link Result} instance.
+	 * @returns a new Result instance that is Ok with the value of the Pipe.
+	 * @remarks
+	 * - this method is equivalent to `Result.Ok(pipe.unwrap())`.
+	 * - You can provide a custom generic type {@typedef E} to the method to create a Result with a custom error type.
+	 * @see {@link Result}
+	 * @see {@link Result.Ok}
+	 *
+	 * @example
+	 * ```typescript
+	 * type User = { name: string };
+	 * declare const user: User;
+	 * const userNameResult = Pipe.create(user).map((user) => user.name).intoResult();
+	 * // 	^? Result<string, never>
+	 */
 	intoResult<E = never>() {
 		return Result.Ok<T, E>(this.value);
 	}
--- src/procedure.ts
+++ src/procedure.ts
@@ -2,22 +2,138 @@ import { Pipe } from "./pipe";
 
 type ProcFn<T, U> = (value: T) => U;
 
+/**
+ * Procedure is a class that represents a lazy flow of data transformation (and effects).
+ * @remarks
+ * - Procedure is similar in nature to {@link Pipe}, but instead of directly running on some value, it creates a workflow that can be run later or composed.
+ * - a Procedure is basically a builder for function composition.
+ *
+ * To create a Procedure, use the static method {@link Procedure.create} followed by chaining the builder methods {@link Procedure.map}, {@link Procedure.effect}, and {@link Procedure.andThen}.
+ * To finalize the Procedure, you can run it using the {@link Procedure.run} and {@link Procedure.runIntoPipe} methods, or you can build a callback function using the {@link Procedure.buildCallback} method.
+ * ```typescript
+ * const addOneAndLog = Procedure.create<number>()
+ * 	.map((value) => value + 1)
+ * 	.effect(console.log);
+ *
+ * const result = addOneAndLog.run(1); // logs(2) and returns 2
+ * ```
+ * ---
+ * Procedures are composable and immutable, meaning you can create new Procedures that are derivatives of existing ones.
+ * For example, you can create a base Procedure `addOne` and then use it to create new procedures that build on top of it.
+ * ```typescript
+ * const addOne = Procedure.create<number>().map((value) => value + 1);
+ * const addOneAndDouble = addOne.map((value) => value * 2);
+ * const addOneAndLog = addOne.effect(console.log);
+ *
+ * const result = addOneAndDouble.run(1); // returns 4
+ * const result2 = addOneAndLog.run(1); // logs(2) and returns 2
+ * ```
+ * ---
+ * Procedures can be composed using the {@link Procedure.andThen} method, which allows you to chain multiple Procedures together.
+ *
+ * ```typescript
+ * const addOne = Procedure.create<number>().map((value) => value + 1);
+ * const double = Procedure.create<number>().map((value) => value * 2);
+ *
+ * const addOneAndDouble = addOne.andThen(double);
+ * const doubleAndAddOne = double.andThen(addOne);
+ *
+ * const result = addOneAndDouble.run(1); // returns 4
+ * const result2 = doubleAndAddOne.run(1); // returns 3
+ * ```
+ * ---
+ * Procedures can be used to build callback functions that can be passed around and run later using the {@link Procedure.buildCallback} method.
+ * ```typescript
+ * function vanillaCallback(value: number) {
+ * 	value += 1;
+ *  value *= 2;
+ *  console.log(value);
+ * 	return value;
+ * }
+ * // is the same as:
+ * const procedureCallback = Procedure.create<number>()
+ * 	.map((value) => value + 1)
+ * 	.map((value) => value * 2)
+ * 	.effect(console.log)
+ * 	.buildCallback();
+ *
+ * const vanillaResult = vanillaCallback(1); // logs(4) and returns 4
+ * const procedureResult = procedureCallback(1); // logs(4) and returns 4
+ */
 export class Procedure<const TIn, const TOut> {
-	private readonly procFn: ProcFn<TIn, TOut>;
+	private constructor(private readonly procFn: ProcFn<TIn, TOut>) {}
 
-	private constructor(procFn: ProcFn<TIn, TOut>) {
-		this.procFn = procFn;
-	}
-
+	/**
+	 * Creates a new Procedure that returns the value it receives.
+	 * @returns a new Procedure that returns the value it receives.
+	 */
 	static create<T>(): Procedure<T, T> {
 		return new Procedure((value: T) => value);
 	}
 
+	/**
+	 * Map is a transformation method that takes a function that transforms the value of the Procedure into a new value.
+	 * @remarks
+	 * - The map method is lazy and does not run the function immediately.
+	 * - The map method is immutable and returns a new Procedure with the transformation applied.
+	 * @param mapFn - A function that transforms the value of the Procedure.
+	 * @returns a new Procedure with the transformation applied.
+	 *
+	 * @example
+	 * ```typescript
+	 * const addOne = Procedure.create<number>().map((value) => value + 1);
+	 * const result = addOne.run(1); // returns 2
+	 *
+	 * // map can be chained
+	 * const addOneAndDouble = Procedure.create<number>()
+	 * 	.map((value) => value + 1)
+	 * 	.map((value) => value * 2);
+	 * const result2 = addOneAndDouble.run(1); // returns 4
+	 */
 	public map<const U>(mapFn: ProcFn<TOut, U>): Procedure<TIn, U> {
 		return new Procedure((value: TIn) => mapFn(this.procFn(value)));
 	}
 
-	public effect(effectFn: (value: TOut) => void): Procedure<TIn, TOut> {
+	/**
+	 * Composes two procedures together, where the output of the first Procedure is the input of the second Procedure.
+	 * @remarks The andThen method is lazy and does not run the Procedures immediately.
+	 * @param nextProcedure - The Procedure to run after the current Procedure.
+	 * @returns a new Procedure that is the composition of the two Procedures (does not mutate the original Procedures).
+	 * @example
+	 * ```typescript
+	 * const addOne = Procedure.create<number>().map((value) => value + 1);
+	 * const double = Procedure.create<number>().map((value) => value * 2);
+	 *
+	 * const addOneAndDouble = addOne.andThen(double);
+	 * console.log(addOneAndDouble.run(1)); // 4
+	 */
+	public andThen<const U>(
+		nextProcedure: Procedure<TOut, U>,
+	): Procedure<TIn, U> {
+		return new Procedure((value: TIn) =>
+			nextProcedure.procFn(this.procFn(value)),
+		);
+	}
+
+	/**
+	 * Runs a side effect function on the value of the Procedure.
+	 * @param effectFn - A function that runs a side effect on the value of the Procedure.
+	 * @remarks
+	 * - The effect method is lazy and does not run the function immediately.
+	 * - The effect method is immutable and returns a new Procedure with the effect applied.
+	 * - the `effectFn` function should not return anything, and mustn't mutate the value.
+	 * @returns a new Procedure with the effect applied.
+	 *
+	 * @example
+	 * ```typescript
+	 * const addOneAndLog = Procedure.create<number>()
+	 * 	.map((value) => value + 1)
+	 * 	.effect(console.log);
+	 * const result = addOneAndLog.run(1); // logs(2) and returns 2
+	 */
+	public effect(
+		effectFn: (value: Readonly<TOut>) => void,
+	): Procedure<TIn, TOut> {
 		return new Procedure((value: TIn) => {
 			const result = this.procFn(value);
 			effectFn(result);
@@ -25,14 +141,52 @@ export class Procedure<const TIn, const TOut> {
 		});
 	}
 
+	/**
+	 * Runs the Procedure on a value and returns the result.
+	 * @param value - The value to run the Procedure on.
+	 * @returns The result of running the Procedure on the value.
+	 * @remarks the run method can be used multiple times on different values as the Procedure is immutable and running it does not change the Procedure.
+	 * @example
+	 * ```typescript
+	 * const addOne = Procedure.create<number>().map((value) => value + 1);
+	 * console.log(addOne.run(1)); // 2
+	 * console.log(addOne.run(2)); // 3
+	 * ```
+	 */
 	public run(value: TIn): TOut {
 		return this.procFn(value);
 	}
 
+	/**
+	 * Runs the Procedure on a value and returns the result as a {@link Pipe}.
+	 * @param value - The value to run the Procedure on.
+	 * @returns A Pipe with the result of running the Procedure on the value.
+	 * @remarks this method is equivalent to calling `Pipe.create(procedure.run(value))`.
+	 * @example
+	 * ```typescript
+	 * const addOne = Procedure.create<number>().map((value) => value + 1);
+	 * const pipe = addOne.runIntoPipe(1);
+	 * console.log(pipe.unwrap()); // 2
+	 * ```
+	 */
 	public runIntoPipe(value: TIn): Pipe<TOut> {
 		return Pipe.create(this.run(value));
 	}
 
+	/**
+	 * Builds a callback function that can be run later.
+	 * @returns a function that can be run later with a value of type `TIn` and returns a value of type `TOut`.
+	 * @remarks The buildCallback method is useful for creating callback functions that can be passed around and run later.
+	 * @example
+	 * ```typescript
+	 * const addOneAndLog = Procedure.create<number>()
+	 * 	.map((value) => value + 1)
+	 * 	.effect(console.log)
+	 * 	.buildCallback();
+	 * const result = addOneAndLog(1); // logs(2) and returns 2
+	 * const result2 = addOneAndLog(2); // logs(3) and returns 3
+	 * ```
+	 */
 	public buildCallback(): ProcFn<TIn, TOut> {
 		return this.procFn;
 	}
